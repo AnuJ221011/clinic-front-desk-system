06b2b78138a7dae6d1f9aa8e2dacd35d581e1d63
Anuj Kumar <anujkum.own@gmail.com>
2025-09-12 18:49:28 +0530

    added backed url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://clinic-front-desk-system.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
